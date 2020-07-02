dedc92a206cf72cb5c990c042b06a5b8db476041
Jamie Cox <jamie@totaldistraction.co.uk>
2020-07-02 21:29:09 +0100

    Base Project Push

---
+++ ATextBasedAdventure/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ATextBasedAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome a Text Based Adventure!");
+            Console.ReadKey();
+        }
+    }
+}
