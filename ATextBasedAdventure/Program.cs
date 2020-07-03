6cbdc788b58be933b45341b3c114661335aab747
Jamie Cox <jamie.cox1995@outlook.com>
2020-07-03 13:15:45 +0100

    CREATE - Created Game, Character, Location, and Ability Classes
    These will be the core classes of the game, they will contain all of the logic and data from the game.

--- ATextBasedAdventure/Program.cs
+++ ATextBasedAdventure/Program.cs
@@ -4,11 +4,23 @@ namespace ATextBasedAdventure
 {
     class Program
     {
+        private static Game _Game;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Welcome a Text Based Adventure!");
-            Console.ReadKey();
+            Initialize();
+
+            while(_Game.IsRunning)
+            {
+                _Game.Update();
+            }
+        }
+
+        private static void Initialize()
+        {
+            Console.Title = "A Text Based Adventure";
+
+            _Game = new Game();
         }
     }
 }
