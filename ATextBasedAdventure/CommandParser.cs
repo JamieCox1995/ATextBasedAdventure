0bba2a0c95ddc702e340554989fa4e045f399ca9
Jamie Cox <jamie@totaldistraction.co.uk>
2020-07-09 07:22:33 +0100

    CREATE - Started to work on the Command Parser to be able to detect user commands

---
+++ ATextBasedAdventure/CommandParser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using ATextBasedAdventure.Entities;
+
+namespace ATextBasedAdventure
+{
+    class CommandParser
+    {
+        public static void ParseCommand(string _InputString, Character _PlayerCharacter)
+        {
+            string[] deliminated = _InputString.Split(new char[2] { ' ', '.' });
+        }
+
+    }
+}
